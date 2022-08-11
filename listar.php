29d71cc2ac2919efe52f216fe7fa94db1e53f058
Lougansh <lougansh@hotmail.com>
2022-08-11 00:42:57 -0300

    Modificado CSS

--- listar.php
+++ listar.php
@@ -1,9 +1,30 @@
 <?php
-session_start();include("conexao.php");include("conf.php");menu();if (isset($_POST['Lista']) && $_POST['Lista'] != ''){$ano = substr($_POST['Lista'],0,1);$turma = substr($_POST['Lista'],1,1);$titulo = $ano.'º Ano '.$turma;
-if($ano=='L') {$sql = "select * from tb_aluno where situacaoMatricula = 'M' order by ano, turma, nome asc";
+session_start();
+include("conexao.php");
+include("conf.php");
+menu();
+if (isset($_POST['Lista']) && $_POST['Lista'] != ''){
+$ano = substr($_POST['Lista'],0,1);
+$turma = substr($_POST['Lista'],1,1);
+$titulo = $ano.'º Ano '.$turma;
+if($ano=='L') {
+$sql = "select * from tb_aluno where situacaoMatricula = 'M' order by ano, turma, nome asc";
 }else{
 	$sql = "select * from tb_aluno where ano = '$ano' and turma = '$turma' and situacaoMatricula = 'M' order by nome asc";
-}$result = mysqli_query($connection, $sql);    while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {		$i++;      $cgm = $row['cgm'];      $ano = $row['ano'];      $turma = $row['turma'];		$nome = $row['nome'];		$nascimento = date("d/m/Y", strtotime($row['nascimento']));		$sexo = $row['sexo'];		$situacao = $row['situacaoMatricula'];		$matriculado = date("d/m/Y", strtotime($row['dataMatricula']));		$observacao = $row['observacao'];		$lista = $lista.'
+}
+$result = mysqli_query($connection, $sql);
+    while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+		$i++;
+      $cgm = $row['cgm'];
+      $ano = $row['ano'];
+      $turma = $row['turma'];
+		$nome = $row['nome'];
+		$nascimento = date("d/m/Y", strtotime($row['nascimento']));
+		$sexo = $row['sexo'];
+		$situacao = $row['situacaoMatricula'];
+		$matriculado = date("d/m/Y", strtotime($row['dataMatricula']));
+		$observacao = $row['observacao'];
+		$lista = $lista.'
 		<tr>
 			<td align="center">'.$cgm.'</td>
 			<td align="center">'.$ano.'</td>
@@ -14,7 +35,19 @@ if($ano=='L') {$sql = "select * from tb_aluno where situacaoMatricula = 'M' ord
 			<td align="center"><a href="Direto.php?id='.$cgm.'" target="_blank" rel="noopener noreferrer">Imprimir</a></td>
 		</tr>
 		';
-		$_SESSION["alunos"] = $lista;    }	$turmaQTDE = $titulo.' - '.$i.' alunos';}		echo '	<html>		<head><title> Lista de alunos '.$ano.'º'.$turma.'</title></head>		<body>				<form method="POST" action="?id=Lista" onchange="form.submit()">				<div align="center"><h2>Editar Alunos'.$mostraCaixaSuspensa.'</h2>'.$turmaQTDE.'<hr width="50%"><hr width="70%"></div>				<div align="center">
+		$_SESSION["alunos"] = $lista;
+
+    }
+	$turmaQTDE = $titulo.' - '.$i.' alunos';
+}	
+	echo '
+	<html>
+		<head><title> Lista de alunos '.$ano.'º'.$turma.'</title></head>
+		<body>	
+			<form method="POST" action="?id=Lista" onchange="form.submit()">
+			<div id="editar">
+				<div align="center"><h2>Editar Alunos'.$mostraCaixaSuspensa.'</h2>'.$turmaQTDE.'<hr width="50%"><hr width="70%"></div>
+				<div align="center">
 				<table border="1">
 				<tr>
 					<td align="center">CGM</td>
@@ -27,4 +60,10 @@ if($ano=='L') {$sql = "select * from tb_aluno where situacaoMatricula = 'M' ord
 				</tr>
 				'.$_SESSION["alunos"].'
 				</table>
-				</div>			</form>		</body>	</html>	';?>
+				</div>
+				</div>
+			</form>
+		</body>
+	</html>
+	';
+?>
