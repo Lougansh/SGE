08db3c519119f0b59f903974a569571e505722e1
Lougansh <lougansh@hotmail.com>
2022-08-12 11:23:27 -0300

    Retirado arquivos inuteis modificado banco e baixado copia banco

--- conexao.php
+++ conexao.php
@@ -1,14 +1,27 @@
-<?php
-$host = "localhost";
-$usuario = "everaldo";
-$senha = "slackware";
-$banco = "adolival";
-
-//$mysqli = new mysqli($host, $usuario, $senha, $banco);
-$connection = mysqli_connect($host, $usuario, $senha, $banco);
-
-if (mysqli_connect_errno()) {
-  echo "Falhou ao conectar ao banco: " . mysqli_connect_error();
-  exit();
-}
+<?php
+
+$host = "localhost";
+
+$usuario = "everaldo";
+
+$senha = "slackware";
+
+$banco = "SGE";
+
+
+
+//$mysqli = new mysqli($host, $usuario, $senha, $banco);
+
+$connection = mysqli_connect($host, $usuario, $senha, $banco);
+
+
+
+if (mysqli_connect_errno()) {
+
+  echo "Falhou ao conectar ao banco: " . mysqli_connect_error();
+
+  exit();
+
+}
+
 ?>
